dbe6a20df025e8c20141daff21d5cf8d19b2432a
Patrick Skiba <skibap@my.easternct.edu>
2016-04-16 13:04:45 -0400

    Registration done, to access go to /accounts/register or any of the other /accounts/* templates

--- books/models.py
+++ books/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     book_title = models.CharField(max_length=250)
     book_author = models.CharField(max_length=150)
     book_isbn = models.CharField(max_length=100)
+    checked_out_by = models.CharField(max_length=250)
 
     def __str__(self):
         return self.book_author
--- libraryApp/urls.py
+++ libraryApp/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^books/', include('books.urls')),
-    url(r'^users/', include('users.urls')),
-    url(r'^library/', include('library.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+
 ]
